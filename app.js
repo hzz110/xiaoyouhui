653873acf8e81943c399d15829ca2fe043af2790
hzz110 <hzz110@gmail.com>
2026-03-28 13:47:17 +0800

    chore: switch api url to custom domain

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ App({
   globalData: {
     userInfo: null,
     // [重要提示] 请将此处替换为您在 Cloudflare Workers 上部署成功后的实际外网请求地址！
-    apiUrl: "https://sjtuaa-backend.hzz110.workers.dev"
+    apiUrl: "https://xiaoyouhui.chuanwenjian.com"
   }
 })
